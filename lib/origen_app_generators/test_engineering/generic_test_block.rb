2b525a01b72dbc25a65d51a62c6cf671bce37a51
Stephen McGinty <stephen.f.mcginty@gmail.com>
2015-07-23 23:25:42 +0100

    Removed forced failure, regression works

--- lib/origen_app_generators/test_engineering/generic_test_block.rb
+++ lib/origen_app_generators/test_engineering/generic_test_block.rb
@@ -12,8 +12,7 @@ module OrigenAppGenerators
       end
 
       def add_requires
-        fail 'yo'
-        # prepend_to_file "lib/#{@name}.rb", "require 'origen_testers'\n"
+        prepend_to_file "lib/#{@name}.rb", "require 'origen_testers'\n"
       end
 
       def conclude
