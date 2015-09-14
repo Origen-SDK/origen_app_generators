a8c33b6397a9cd12e74e7e8552bdfb5e010fe17d
Stephen McGinty <stephen.f.mcginty@gmail.com>
2015-09-14 15:29:44 +0100

    Comment out web page deploy by default

--- templates/app_generators/application/config/application.rb
+++ templates/app_generators/application/config/application.rb
@@ -64,12 +64,12 @@ class <%= @namespace %>Application < Origen::Application
   #end
  
   # This will automatically deploy your documentation after every tag
-  def after_release_email(tag, note, type, selector, options)
-    command = "origen web compile --remote --api"
-    Dir.chdir Origen.root do
-      system command
-    end
-  end
+  #def after_release_email(tag, note, type, selector, options)
+  #  command = "origen web compile --remote --api"
+  #  Dir.chdir Origen.root do
+  #    system command
+  #  end
+  #end
 
   # Ensure that all tests pass before allowing a release to continue
   #def validate_release
