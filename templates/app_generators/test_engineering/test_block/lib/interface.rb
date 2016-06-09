c89506f6b8558777aa6e8204d41ff7caf4cb251c
Stephen McGinty <stephen.f.mcginty@gmail.com>
2016-06-09 08:53:15 +0100

    Update interface.rb

--- templates/app_generators/test_engineering/test_block/lib/interface.rb
+++ templates/app_generators/test_engineering/test_block/lib/interface.rb
@@ -1,5 +1,21 @@
 module <%= @namespace %>
   class Interface
     include OrigenTesters::ProgramGenerators
+    
+    # This will be called at the start of every flow or sub-flow (whenever Flow.create
+    # is called).
+    # Any options passed to Flow.create will be passed in here.
+    # The options will contain top_level: true, whenever this is called at the start of
+    # a new top-level flow.
+    def startup(options = {})
+    end
+
+    # This will be called at the end of every flow or sub-flow (at the end of every
+    # Flow.create block).
+    # Any options passed to Flow.create will be passed in here.
+    # The options will contain top_level: true, whenever this is called at the end of a
+    # top-level flow file.
+    def shutdown(options = {})
+    end
   end
 end
