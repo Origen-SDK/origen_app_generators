5ba9d89528513a3872f8bb6f32997cb9c4c37cd4
Caquelin, Brian <Brian.Caquelin@amd.com>
2020-12-03 08:40:26 -0600

    Updated app version and history

--- config/version.rb
+++ config/version.rb
@@ -1,7 +1,7 @@
 module OrigenAppGenerators
   MAJOR = 2
-  MINOR = 1
-  BUGFIX = 1
+  MINOR = 2
+  BUGFIX = 0
   DEV = nil
   VERSION = [MAJOR, MINOR, BUGFIX].join(".") + (DEV ? ".pre#{DEV}" : '')
 end
